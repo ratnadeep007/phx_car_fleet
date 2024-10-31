8d00e045b0bdc9b6c62d7b53e9a677a50d1e51eb
Ratnadeep Bhattacharyya <ratnadeep@tarkalabs.com>
2024-10-31 13:28:51 +0530

    booking route

--- lib/phx_practice_web/controllers/book_controller.ex
+++ lib/phx_practice_web/controllers/book_controller.ex
@@ -1,10 +1,11 @@
 defmodule PhxPracticeWeb.BookController do
   use PhxPracticeWeb, :controller
 
+  alias PhxPractice.Fleet
+
   def index(conn, _params) do
-    # IO.inspect(conn.request_path)
-    # assign(conn, :name, "test")
-    # conn.assign(:name, "test")
-    render(conn, :home, name: "test")
+    fleet_cars = Fleet.list_fleet_cars()
+    total = fleet_cars |> Enum.count()
+    render(conn, :home, fleet_cars: fleet_cars, total_cars: total)
   end
 end
